e8c9aa983c45a7ff1dc80deab7bb54b767c28e6e
Hefler <heflerdev@gmail.com>
2021-03-22 19:43:20 -0300

    Update: File Structure

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import App from 'App.js';
+import App from './App.js';
 
 const Routes = () => (
     <BrowserRouter>
--- src/reducers/stocksReducer.js
+++ src/reducers/stocksReducer.js
@@ -5,4 +5,4 @@ function stocksReducer(state = [], action) {
   }
 }
 
-
+export default stocksReducer;
