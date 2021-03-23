3ce442503effc9c38bec2a7a94b66d3bf683a58b
Hefler <heflerdev@gmail.com>
2021-03-23 20:55:58 -0300

    Update: combine reducers

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,13 @@
-import { createStore } from 'redux';
+import { combineReducers, createStore } from 'redux';
 import stocksReducer from '../reducers/stocksReducer';
+import filterReducer from '../reducers/filterReducer';
 
-const store = createStore(stocksReducer);
+const rootReducer = combineReducers({
+  stocksReducer,
+  filterReducer,
+});
+
+const store = createStore(rootReducer);
 store.subscribe(() => console.log(store.getState));
 
 export default store;
