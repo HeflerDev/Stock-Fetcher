3587b3d2b1396539b67cd6065f0c8be0ce008bcf
Hefler <heflerdev@gmail.com>
2021-03-25 18:21:39 -0300

    Add Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav className="board between">
+      <Link to="/about" className="col-6 header-button">
+        About the Dev
+      </Link>
+      <Link to="/" className="col-6 header-button">
+        Search
+      </Link>
+    </nav>
+  )
+}
+
+export default Navbar;
